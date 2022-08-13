3aab853df8ff5b8d79184f27bac5db11a65ec7fa
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2022-08-13 12:22:10 -0300

    Adicionado os repostiories de Usuario

---
+++ CadastroUsuario/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using CadastroUsuario.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroUsuario.Repositories
+{
+    interface IUsuarioRepository
+    {
+        Task<IEnumerable<Usuario>> Get();
+        Task<Usuario> Get(string id);
+        Task Create(Usuario usuario);
+        Task Update(Usuario usuario);
+        Task Delete(string id);
+
+    }
+}
---
+++ CadastroUsuario/Repositories/UsuarioRepository.cs
@@ -0,0 +1,36 @@
+﻿using CadastroUsuario.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroUsuario.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public Task Create(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Usuario>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
