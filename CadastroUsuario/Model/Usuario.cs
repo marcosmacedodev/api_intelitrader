1a2cdb4432f2174b94e7e867954377f5f1ec1c6e
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2022-08-13 12:08:08 -0300

    Criado a classe Usuario, e os campos Id(string), FirstName(string), Surname(string), Age(int), CreationDate(DateTime)

---
+++ CadastroUsuario/Model/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroUsuario.Model
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
